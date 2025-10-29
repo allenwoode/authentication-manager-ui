471ae040231873135173f1d4b1f1be4ea4a95aae
allenwu <allenwogpt@gmail.com>
2025-10-29 19:03:03 +0800

    feat: customer

---
+++ views/system/Note/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <j-page-container>
+        <h1>Note Page Index</h1>
+        <div>
+        {{ route.name }}
+        </div>
+        <j-permission-button
+        type="primary"
+        hasPermission="system/Note:add"
+        @click="() => { status = status == 1 ? 0 : 1 }"
+        >
+            <template #icon>
+                123456
+            </template>
+            {{ status }}
+        </j-permission-button>
+    </j-page-container>
+</template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const status = ref(1)
+</script>
---
+++ views/system/Relationship/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <j-page-container>
+    <div>
+      {{ route.name }}
+    </div>
+    <j-permission-button
+      type="primary"
+      hasPermission="system/Relationship:add"
+      @click="() => { status = status == 1 ? 0 : 1 }"
+    >
+      <template #icon>
+        
+      </template>
+
+    </j-permission-button>
+
+    <permission-button
+      type="primary"
+      :tooltip="{
+        title: '测试'
+      }"
+    >
+
+      你好 {{ status }}
+    </permission-button>
+    <br/>
+
+  </j-page-container>
+  
+  <div style="width: 100%; height: 300px">
+    <init-Amap
+      @dragstart="mapClick"
+      mapStyle="dark"
+    >
+      <el-amap-marker
+        :position="[0,0]"
+      />
+    </init-Amap>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const status = ref(1)
+
+const mapClick = (e: any) => {
+  console.log(e)
+}
+</script>
+
+<style scoped>
+
+</style>
